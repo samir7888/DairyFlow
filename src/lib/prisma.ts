015eb8ff1a85ee8a018ee342b2ca2118bc82cf45
samir7888 <basnetsameer78@gmail.com>
2026-08-18 10:26:09 +0545

    fix

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,22 +1,20 @@
+import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 
-function createPrismaClient() {
-  const pool = new pg.Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
-  const adapter = new PrismaPg(pool);
-  // Inline require avoids TS module resolution issues with Prisma 7's generated client
-  // eslint-disable-next-line @typescript-eslint/no-require-imports
-  const { PrismaClient } = require("@prisma/client");
-  return new PrismaClient({ adapter }) as InstanceType<typeof import(".prisma/client").PrismaClient>;
-}
-
-type PrismaClientInstance = ReturnType<typeof createPrismaClient>;
+type PrismaClientInstance = InstanceType<typeof PrismaClient>;
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClientInstance | undefined;
 };
+
+function createPrismaClient(): PrismaClientInstance {
+  const pool = new pg.Pool({
+    connectionString: process.env.DATABASE_URL,
+  });
+  const adapter = new PrismaPg(pool);
+  return new PrismaClient({ adapter });
+}
 
 export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
